1ed1867f43673dbc6c573e67e0df0e7c01c8b853
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-06-24 10:56:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
  fetch('https://anapioficeandfire.com/api/books')
   .then(response => response.json())
-  .then(data => renderBooks(data))
+  .then(data => function(){
+    renderBooks(data)
+  })
 }
 
 function renderBooks(books) {
